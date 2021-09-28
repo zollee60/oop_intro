7d5cb4e2753ba2839561042520f6ccc920f75fd9
Tóth Márk Zoltán <zollee60@gmail.com>
2021-09-28 18:57:02 +0200

    Add project files.

---
+++ oop_intro/Ember.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_intro
+{
+    class Ember
+    {
+        /*
+        // Attribútumok
+
+        private string _nev;
+        private int kor;
+        private int magassag;
+
+        // Propertyk
+        public string Nev { get => _nev; set => _nev = value; }
+        public int Kor { get => kor; set => kor = value; }
+        public int Magassag { get => magassag; set => magassag = value; }
+        */
+
+        // Property + Attribútum
+        public string Nev { get; set; }
+        public int Kor { get; set; }
+        public int Magassag { get; set; }
+
+        // Konstruktor: Osztály -> Objektum (Az objektum az adott osztály példánya)
+        public Ember(string nev, int kor, int magassag)
+        {
+            Nev = nev;
+            Kor = kor;
+            Magassag = magassag;
+        }
+
+        // Metódusok
+        public void Koszones()
+        {
+            Console.WriteLine($"Szia {Nev}!");
+        }
+
+        // Nevezetes, fontos metódusok
+        public override string ToString()
+        {
+            return $"Név: {Nev}, kor: {Kor}, magasság: {Magassag}cm";
+        }
+
+        
+
+    }
+}
---
+++ oop_intro/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ember jani = new Ember("Jani", 20, 187);
+            jani.Koszones();
+
+            Console.WriteLine(jani);
+        }
+    }
+}
